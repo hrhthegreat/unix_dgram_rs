8d5dd3815a23bae69cbabad85cb2f21f36421f91
hrhthegreat <hamidreza.hanafi@gmail.com>
2026-05-31 23:32:07 -0400

    Add TypeScript declarations

---
+++ test/package-metadata.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const root = path.join(__dirname, '..');
+const pkg = require('../package.json');
+
+test('package publishes its TypeScript declarations', () => {
+  assert.equal(pkg.types, 'index.d.ts');
+  assert.ok(pkg.files.includes(pkg.types));
+  assert.ok(fs.existsSync(path.join(root, pkg.types)));
+});
